7024f779d2fbd2c7bc045ea5617e5f20b0de4ccd
cilansSlodhi <cilanssugriv@gmail.com>
2025-11-12 17:03:48 +0530

    change end point of Api

--- app/services/api.ts
+++ app/services/api.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://todo-backend-env.eba-33cyikmh.ap-northeast-3.elasticbeanstalk.com/api';
 
 export interface Todo {
   id: string;
