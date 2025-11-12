b91a03ccbd5bf72cc40146db904e8a12958a335a
cilansSlodhi <cilanssugriv@gmail.com>
2025-11-12 17:14:05 +0530

    use Reverse prosy to handle http request

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:5000/api/:path*',
+        destination: 'http://todo-backend-env.eba-33cyikmh.ap-northeast-3.elasticbeanstalk.com/api/:path*',
       },
     ];
   },
